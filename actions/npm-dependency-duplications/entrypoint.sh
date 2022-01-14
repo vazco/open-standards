81c8f1fef7574741bf5a460308402740f1008a86
Konrad Bosak <konrad.bosak@protonmail.com>
2022-01-14 17:11:09 +0100

    Change duplication detection method

--- actions/npm-dependency-duplications/entrypoint.sh
+++ actions/npm-dependency-duplications/entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh -l
 
 cd "$1" || exit
-output=npm find-dupes --fund=false
-if [[ $string == *"up to date"* ]]; then
+cp package-lock.json package-lock-copy.json
+npm dedupe 1> /dev/null
+diff=`diff package-lock.json package-lock-copy.json`
+if [[ $diff != "" ]]; then
   echo "::"warning file=package-lock.json"::"Duplicate dependencies found. Run npm dedupe to remove them, or add --dry-run parameter to get more info.
 fi
+rm package-lock-copy.json
