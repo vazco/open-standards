3355126be6206075ba650bbd91eeb643e8d63c71
Konrad Bosak <konrad.bosak@protonmail.com>
2022-01-14 17:51:27 +0100

    Reformat check script

--- actions/npm-dependency-duplications/entrypoint.sh
+++ actions/npm-dependency-duplications/entrypoint.sh
@@ -3,8 +3,8 @@
 cd "$1" || exit
 cp package-lock.json package-lock-copy.json
 npm dedupe 1> /dev/null
-diff=`diff package-lock.json package-lock-copy.json`
-if [[ $diff != "" ]]; then
+diffRes=$(diff package-lock.json package-lock-copy.json)
+if [ "$diffRes" != "" ]; then
   echo "::"warning file=package-lock.json"::"Duplicate dependencies found. Run npm dedupe to remove them, or add --dry-run parameter to get more info.
 else
    echo "No dependency duplication found."
