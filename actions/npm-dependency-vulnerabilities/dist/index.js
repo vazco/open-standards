43780995c11c0c80d6424e825768776cf7c2cbe7
Konrad Bosak <konrad.bosak@vazco.eu>
2022-04-29 15:26:39 +0200

    update dist

--- actions/npm-dependency-vulnerabilities/dist/index.js
+++ actions/npm-dependency-vulnerabilities/dist/index.js
@@ -2971,25 +2971,28 @@ const core = __nccwpck_require__(186);
 const exec = __nccwpck_require__(514);
 
 async function main() {
-    try {
-        const path = core.getInput('path', {required: true});
+  try {
+    const path = core.getInput('path', { required: true });
 
-        const {exitCode, stdout: auditOutput} = await exec.getExecOutput(`npm audit --dry-run`, [], {
-            cwd: path ? path : undefined,
-            ignoreReturnCode: true
-        })
+    const { exitCode } = await exec.getExecOutput('npm audit --dry-run', [], {
+      cwd: path ? path : undefined,
+      ignoreReturnCode: true,
+    });
 
-        if (exitCode === 0) {
-            core.info('No vulnerabilities found.')
-        } else {
-            core.warning(`Dependency vulnerabilities found. Run npm audit to get more info.`, {file: `${path}/package-lock.json`})
-        }
-    } catch (error) {
-        core.warning(error.message);
+    if (exitCode === 0) {
+      core.info('No vulnerabilities found.');
+    } else {
+      core.warning(
+        'Dependency vulnerabilities found. Run npm audit to get more info.',
+        { file: `${path}/package-lock.json` },
+      );
     }
+  } catch (error) {
+    core.warning(error.message);
+  }
 }
 
-main()
+main();
 
 })();
 
